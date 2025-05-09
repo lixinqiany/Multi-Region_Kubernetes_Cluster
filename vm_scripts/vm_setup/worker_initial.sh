b557e6ea5798679c90dcb1e9091b803c52f66428
lixinqiany <lixinqiany@student.unimelb.edu.au>
2025-05-09 13:32:14 +0800

    vm-setup中的启动脚本全设置sudo

--- vm_scripts/vm_setup/worker_initial.sh
+++ vm_scripts/vm_setup/worker_initial.sh
@@ -18,25 +18,25 @@ sudo timedatectl set-timezone Australia/Melbourne
 
 sudo mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo \
+sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y containerd.io
 
 sudo mkdir -p /etc/containerd
-containerd config default > /etc/containerd/config.toml
+sudo containerd config default > /etc/containerd/config.toml
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 
-systemctl restart containerd
-systemctl enable containerd
+sudo systemctl restart containerd
+sudo systemctl enable containerd
 #systemctl status containerd
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nfs-common rpcbind
@@ -44,6 +44,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nfs-common rpcbind
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y kubelet=1.32.3-1.1 kubeadm=1.32.3-1.1 kubectl=1.32.3-1.1
 sudo DEBIAN_FRONTEND=noninteractive apt-mark hold kubelet kubeadm kubectl
 
-systemctl enable kubelet
+sudo systemctl enable kubelet
 
-kubeadm join 10.192.0.2:6443 --token 2tk7uf.r5lg4pr4xln8ap4i --discovery-token-ca-cert-hash sha256:01d90eede05983350531420a401e040560870f36ab8ddb9e031936107a906ac1
+sudo kubeadm join 10.192.0.2:6443 --token 2tk7uf.r5lg4pr4xln8ap4i --discovery-token-ca-cert-hash sha256:01d90eede05983350531420a401e040560870f36ab8ddb9e031936107a906ac1
