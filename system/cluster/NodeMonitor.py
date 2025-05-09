6a5979091d42a349b0340b40ac4e3416c4c695ec
lixinqiany <lixinqiany@student.unimelb.edu.au>
2025-05-09 17:18:33 +0800

    用英文输出csv

--- system/cluster/NodeMonitor.py
+++ system/cluster/NodeMonitor.py
@@ -113,13 +113,13 @@ class NodeMonitor:
             curr_nodes = set(usage_cpu.keys())
             added = curr_nodes - self.prev_nodes
             removed = self.prev_nodes - curr_nodes
-            added_str = ";".join(f"{n} 节点被新增" for n in added) if added else ""
-            removed_str = ";".join(f"{n} 节点被移除" for n in removed) if removed else ""
+            added_str = "//".join(f"{n} is added" for n in added) if added else ""
+            removed_str = "//".join(f"{n} is removed" for n in removed) if removed else ""
             cluster_line = [timestamp,
                             str(len(curr_nodes)),
                             added_str,
                             removed_str,
-                            ";".join(curr_nodes)]
+                            "//".join(curr_nodes)]
             self.write_csv("data/node/cluster-info.csv", cluster_header, cluster_line)
             if added or removed:
                 self.logger.info(f"Cluster Change at {timestamp}: 新增节点={added}, 移除节点={removed}")
